0545704ac2b9c156611429c9edaaa1686aba3ccf
Anthony larez <ajlarezm@gmail.com>
2025-05-03 11:55:23 -0400

    arreglo de url en auth

--- auth/urls.py
+++ auth/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import SignUpView,home # importamos la vista de registro de usuarios
 
 urlpatterns = [
-    path("/auth", include("django.contrib.auth.urls")), # urls de autenticacion de django
+    path("auth/", include("django.contrib.auth.urls")), # urls de autenticacion de django
     path('signup/', SignUpView.as_view(), name='signup'), # url para el registro de usuarios
     path('', home, name='home'), # url para la pagina de inicio
 
